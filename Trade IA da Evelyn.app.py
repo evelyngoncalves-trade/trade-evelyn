4dc6b7e1e243b9fba4ea9878e4937e2c0243c38b
Trade IA da Evelyn <evelyngoncalvess05@gmail.com>
2026-08-05 12:33:18 -0300

    Update Trade IA da Evelyn.app.py

--- Trade IA da Evelyn.app.py
+++ Trade IA da Evelyn.app.py
@@ -89,7 +89,7 @@ with col_ia:
                     )
 
                     response = client.models.generate_content(
-                        model='gemini-2.5-flash',
+                        model='gemini-2.0-flash',
                         contents=[prompt, image]
                     )
 
